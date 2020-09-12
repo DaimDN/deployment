5f7980e08e6b315d3af0351d38205cef3643cec9
unknown <you@example.com>
2020-09-12 09:56:52 +0100

    first time app build

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const app = express();
+const port = 3000;
+const parser = require('body-parser');
+
+
+var array = [];
+
+app.use(parser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res)=>{
+    res.render('index', {data: array});
+})
+
+app.post('/add', (req, res)=>{
+    var query = req.body;
+    
+    array.push(query);
+
+    res.redirect('/');
+})
+
+app.listen(port, (req, res)=>{
+    console.log("Server is up and running on port "  + port);
+})
